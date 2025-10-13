11b8def1765b121441d1c739a957b668abd83ed0
dm-vev <80632449+dm-vev@users.noreply.github.com>
2025-10-13 09:07:13 +0300

    Constrain pmgen populators to their generating chunk

--- server/world/generator/pmgen/populate/tree.go
+++ server/world/generator/pmgen/populate/tree.go
@@ -20,24 +20,28 @@ func (t Tree) Populate(w *world.World, pos world.ChunkPos, _ *chunk.Chunk, r *ra
 		for i := int32(0); i < amount; i++ {
 			x := int(r.Range(pos[0]*16, pos[0]*16+15))
 			z := int(r.Range(pos[1]*16, pos[1]*16+15))
-			if y, ok := t.highestWorkableBlock(tx, x, z); ok {
+			if y, ok := t.highestWorkableBlock(tx, pos, x, z); ok {
 				treeType := t.Type
 				if birch, ok := treeType.(BirchTree); ok && r.Int31n(39) == 0 {
 					birch.Super = true
 					treeType = birch
 				}
-				treeType.Grow(tx, cube.Pos{x, y, z}, r)
+				treeType.Grow(tx, pos, cube.Pos{x, y, z}, r)
 			}
 		}
 	})
 }
 
-func (t Tree) highestWorkableBlock(tx *world.Tx, x, z int) (int, bool) {
+func (t Tree) highestWorkableBlock(tx *world.Tx, chunkPos world.ChunkPos, x, z int) (int, bool) {
 	for y := 127; y >= 0; y-- {
-		b := tx.Block(cube.Pos{x, y - 1, z})
-		if (b == block.Dirt{} || b == block.Grass{}) {
+		below := cube.Pos{x, y - 1, z}
+		if !inChunk(below, chunkPos) {
+			continue
+		}
+		b := tx.Block(below)
+		if b == (block.Dirt{}) || b == (block.Grass{}) {
 			return y, true
-		} else if (b != block.Air{}) {
+		} else if b != (block.Air{}) {
 			return 0, false
 		}
 	}
@@ -50,13 +54,13 @@ var overridable = map[world.Block]struct{}{
 }
 
 type TreeType interface {
-	Grow(tx *world.Tx, pos cube.Pos, r *rand.Random)
+	Grow(tx *world.Tx, chunkPos world.ChunkPos, pos cube.Pos, r *rand.Random)
 }
 
 type SpruceTree struct{}
 
-func (SpruceTree) Grow(tx *world.Tx, pos cube.Pos, r *rand.Random) {
-	if !canGrow(tx, pos, 10) {
+func (SpruceTree) Grow(tx *world.Tx, chunkPos world.ChunkPos, pos cube.Pos, r *rand.Random) {
+	if !canGrow(tx, chunkPos, pos, 10) {
 		return
 	}
 	treeHeight := int(r.Int31n(4) + 6)
@@ -64,7 +68,7 @@ func (SpruceTree) Grow(tx *world.Tx, pos cube.Pos, r *rand.Random) {
 	topSize := treeHeight - int(1+r.Int31n(2))
 	lr := 2 + int(r.Int31n(2))
 
-	trunk(tx, pos, block.SpruceWood(), treeHeight-int(r.Int31n(3)))
+	trunk(tx, chunkPos, pos, block.SpruceWood(), treeHeight-int(r.Int31n(3)))
 
 	radius := int(r.Int31n(2))
 	minR, maxR := 0, 1
@@ -80,7 +84,10 @@ func (SpruceTree) Grow(tx *world.Tx, pos cube.Pos, r *rand.Random) {
 				}
 
 				p := cube.Pos{x, yy, z}
-				if b := tx.Block(p); (b.Model() != model.Solid{}) {
+				if !inChunk(p, chunkPos) {
+					continue
+				}
+				if b := tx.Block(p); b.Model() != (model.Solid{}) {
 					tx.SetBlock(p, block.Leaves{Wood: block.SpruceWood()}, nil)
 				}
 			}
@@ -100,32 +107,32 @@ func (SpruceTree) Grow(tx *world.Tx, pos cube.Pos, r *rand.Random) {
 
 type OakTree struct{}
 
-func (OakTree) Grow(tx *world.Tx, pos cube.Pos, r *rand.Random) {
-	if !canGrow(tx, pos, 7) {
+func (OakTree) Grow(tx *world.Tx, chunkPos world.ChunkPos, pos cube.Pos, r *rand.Random) {
+	if !canGrow(tx, chunkPos, pos, 7) {
 		return
 	}
 	treeHeight := int(r.Int31n(3)) + 4
-	basicTop(tx, pos, r, block.Leaves{Wood: block.OakWood()}, treeHeight)
-	trunk(tx, pos, block.OakWood(), treeHeight-1)
+	basicTop(tx, chunkPos, pos, r, block.Leaves{Wood: block.OakWood()}, treeHeight)
+	trunk(tx, chunkPos, pos, block.OakWood(), treeHeight-1)
 }
 
 type BirchTree struct {
 	Super bool
 }
 
-func (b BirchTree) Grow(tx *world.Tx, pos cube.Pos, r *rand.Random) {
-	if !canGrow(tx, pos, 7) {
+func (b BirchTree) Grow(tx *world.Tx, chunkPos world.ChunkPos, pos cube.Pos, r *rand.Random) {
+	if !canGrow(tx, chunkPos, pos, 7) {
 		return
 	}
 	treeHeight := int(r.Int31n(3)) + 5
 	if b.Super {
 		treeHeight += 5
 	}
-	basicTop(tx, pos, r, block.Leaves{Wood: block.BirchWood()}, treeHeight)
-	trunk(tx, pos, block.BirchWood(), treeHeight-1)
+	basicTop(tx, chunkPos, pos, r, block.Leaves{Wood: block.BirchWood()}, treeHeight)
+	trunk(tx, chunkPos, pos, block.BirchWood(), treeHeight-1)
 }
 
-func basicTop(tx *world.Tx, pos cube.Pos, r *rand.Random, leaves block.Leaves, treeHeight int) {
+func basicTop(tx *world.Tx, chunkPos world.ChunkPos, pos cube.Pos, r *rand.Random, leaves block.Leaves, treeHeight int) {
 	for yy := pos[1] - 3 + treeHeight; yy <= pos[1]+treeHeight; yy++ {
 		yOff := yy - (pos[1] + treeHeight)
 		mid := 1 - yOff/2
@@ -136,26 +143,36 @@ func basicTop(tx *world.Tx, pos cube.Pos, r *rand.Random, leaves block.Leaves, t
 				if xOff == mid && zOff == mid && (yOff == 0 || r.Int31n(2) == 0) {
 					continue
 				}
-				if (tx.Block(cube.Pos{xx, yy, zz}).Model() != model.Solid{}) {
-					tx.SetBlock(cube.Pos{xx, yy, zz}, leaves, nil)
+				p := cube.Pos{xx, yy, zz}
+				if !inChunk(p, chunkPos) {
+					continue
+				}
+				if tx.Block(p).Model() != (model.Solid{}) {
+					tx.SetBlock(p, leaves, nil)
 				}
 			}
 		}
 	}
 }
 
-func trunk(tx *world.Tx, pos cube.Pos, wood block.WoodType, trunkHeight int) {
-	tx.SetBlock(pos.Sub(cube.Pos{0, 1}), block.Dirt{}, nil)
+func trunk(tx *world.Tx, chunkPos world.ChunkPos, pos cube.Pos, wood block.WoodType, trunkHeight int) {
+	base := pos.Sub(cube.Pos{0, 1})
+	if inChunk(base, chunkPos) {
+		tx.SetBlock(base, block.Dirt{}, nil)
+	}
 
 	for y := 0; y < trunkHeight; y++ {
-		b := tx.Block(pos.Add(cube.Pos{0, y}))
-		if _, ok := overridable[b]; ok {
-			tx.SetBlock(pos.Add(cube.Pos{0, y}), block.Log{Wood: wood}, nil)
+		p := pos.Add(cube.Pos{0, y})
+		if !inChunk(p, chunkPos) {
+			continue
+		}
+		if _, ok := overridable[tx.Block(p)]; ok {
+			tx.SetBlock(p, block.Log{Wood: wood}, nil)
 		}
 	}
 }
 
-func canGrow(tx *world.Tx, pos cube.Pos, treeHeight int) bool {
+func canGrow(tx *world.Tx, chunkPos world.ChunkPos, pos cube.Pos, treeHeight int) bool {
 	radiusToCheck := 0
 	for yy := 0; yy < treeHeight+3; yy++ {
 		if yy == 1 || yy == treeHeight {
@@ -163,7 +180,11 @@ func canGrow(tx *world.Tx, pos cube.Pos, treeHeight int) bool {
 		}
 		for xx := -radiusToCheck; xx <= radiusToCheck; xx++ {
 			for zz := -radiusToCheck; zz <= radiusToCheck; zz++ {
-				if _, ok := overridable[tx.Block(cube.Pos{pos[0] + xx, pos[1] + yy, pos[2] + zz})]; !ok {
+				p := cube.Pos{pos[0] + xx, pos[1] + yy, pos[2] + zz}
+				if !inChunk(p, chunkPos) {
+					continue
+				}
+				if _, ok := overridable[tx.Block(p)]; !ok {
 					return false
 				}
 			}
