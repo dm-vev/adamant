ef5c244f6f48043bed1f95a7809ba2c9d7b722e7
dm-vev <80632449+dm-vev@users.noreply.github.com>
2025-10-13 09:07:13 +0300

    Constrain pmgen populators to their generating chunk

---
+++ server/world/generator/pmgen/populate/util.go
@@ -0,0 +1,10 @@
+package populate
+
+import (
+	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/world"
+)
+
+func inChunk(pos cube.Pos, chunkPos world.ChunkPos) bool {
+	return int32(pos[0]>>4) == chunkPos[0] && int32(pos[2]>>4) == chunkPos[1]
+}
