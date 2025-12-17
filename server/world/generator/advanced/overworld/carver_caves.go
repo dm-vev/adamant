c0c6d972a6a3cc5cbe8886db18bfb20befd3c8d8
dm-vev <carmakdoom@gmail.com>
2025-12-17 12:50:56 +0300

    continue gen porting

--- server/world/generator/advanced/overworld/carver_caves.go
+++ server/world/generator/advanced/overworld/carver_caves.go
@@ -149,6 +149,16 @@ func (g *Overworld) carveCaveTunnel(seed int64, chunkX, chunkZ int, c *chunk.Chu
 				if yEnd > 248 {
 					yEnd = 248
 				}
+				ryMin, ryMax := int(c.Range().Min()), int(c.Range().Max())
+				if yStart < ryMin {
+					yStart = ryMin
+				}
+				if yEnd > ryMax {
+					yEnd = ryMax
+				}
+				if yEnd < yStart {
+					continue
+				}
 
 				if g.caveHitsWater(c, xStart, xEnd, yStart, yEnd, zStart, zEnd) {
 					continue
