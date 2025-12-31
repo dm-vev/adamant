62721251f9fdfcdd8190ab84fd351b361b9f0fd5
dm-vev <carmakdoom@gmail.com>
2025-12-31 19:40:32 +0300

    fix: consume bone meal after grass growth

--- server/block/grass.go
+++ server/block/grass.go
@@ -79,6 +79,7 @@ func (g Grass) RandomTick(pos cube.Pos, tx *world.Tx, r *rand.Rand) {
 
 // BoneMeal ...
 func (g Grass) BoneMeal(pos cube.Pos, tx *world.Tx) bool {
+	applied := false
 	for i := 0; i < 14; i++ {
 		c := pos.Add(cube.Pos{rand.IntN(6) - 3, 0, rand.IntN(6) - 3})
 		above := c.Side(cube.FaceUp)
@@ -86,10 +87,12 @@ func (g Grass) BoneMeal(pos cube.Pos, tx *world.Tx) bool {
 		_, grass := tx.Block(c).(Grass)
 		if air && grass {
 			tx.SetBlock(above, plantSelection[rand.IntN(len(plantSelection))], nil)
+			applied = true
 		}
 	}
 
-	return false
+	// Only consume bone meal if it successfully places a plant.
+	return applied
 }
 
 // BreakInfo ...
