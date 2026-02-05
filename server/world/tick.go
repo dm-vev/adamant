4cf44a802c607d00f5ceb79d7eb501dc50a26beb
Alexander <123891354+FDUTCH@users.noreply.github.com>
2026-02-05 14:50:18 +0000

    world/world.go: Fixed spawn points in other dimensions, closes #1102. (#1143)

--- server/world/tick.go
+++ server/world/tick.go
@@ -91,7 +91,7 @@ func (t ticker) tick(tx *Tx) {
 	defer w.releaseViewers(viewers)
 
 	w.set.Lock()
-	if s := w.set.Spawn; s[1] > tx.Range()[1] {
+	if s := w.set.Spawn; s[1] > tx.Range()[1] && w.Dimension() == Overworld {
 		// Vanilla will set the spawn position's Y value to max to indicate that
 		// the player should spawn at the highest position in the world.
 		w.set.Spawn[1] = w.highestObstructingBlock(s[0], s[2]) + 1
