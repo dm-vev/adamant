1632ffa38db7e4a0b7dd9e98d96c50e8712525c0
Dasciam <122279000+Dasciam@users.noreply.github.com>
2026-06-01 22:24:49 +0300

    block/grass.go: Fix bone meal duplication glitch (#1266)

--- server/block/grass.go
+++ server/block/grass.go
@@ -1,9 +1,10 @@
 package block
 
 import (
+	"math/rand/v2"
+
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/world"
-	"math/rand/v2"
 )
 
 // Grass blocks generate abundantly across the surface of the world.
