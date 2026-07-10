df-mc/dragonfly

server/world: add per-viewer view layers (#1223)
* server/world: add per-viewer view layers

* add scoretags

* various changes

* Update view_layer.go

* various changes

* Update session.go

* Update view_layer.go

* Update view_layer.go

* add comments

* various changes

* various changes

* Update world.go

* Update server/player/player.go

Co-authored-by: Hashim <hashimssoftware@gmail.com>

* Update server/player/player.go

Co-authored-by: Hashim <hashimssoftware@gmail.com>

* Update server/player/player.go

Co-authored-by: Hashim <hashimssoftware@gmail.com>

* use unsetflag

* Update go.sum

* Update view_layer.go

* various changes

* Update view_layer.go

* Update view_layer.go

---------

Co-authored-by: RestartFU <me@restartfu.com>
Co-authored-by: Hashim <hashimssoftware@gmail.com>

server/session/session_list.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/df-mc/dragonfly/server/internal/sliceutil"
 	"github.com/df-mc/dragonfly/server/player/skin"
+	"github.com/df-mc/dragonfly/server/world"
 	"github.com/google/uuid"
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
@@ -33,14 +34,23 @@ func (l *sessionList) Add(s *Session) {
 	l.s = append(l.s, s)
 }
 
-func (l *sessionList) Remove(s *Session) {
+func (l *sessionList) Remove(s *Session, entity world.Entity) {
 	l.mu.Lock()
-	defer l.mu.Unlock()
-
+	removedFrom := slices.Clone(l.s)
 	for _, other := range l.s {
 		l.unsendSessionFrom(s, other)
 	}
 	l.s = sliceutil.DeleteVal(l.s, s)
+	l.mu.Unlock()
+
+	if entity == nil {
+		return
+	}
+	for _, other := range removedFrom {
+		if other.viewLayer != nil {
+			other.viewLayer.Remove(entity)
+		}
+	}
 }
 
 func (l *sessionList) Lookup(id uuid.UUID) (*Session, bool) {

dm-vev/adamant

server/session/session_list.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/df-mc/dragonfly/server/internal/sliceutil"
 	"github.com/df-mc/dragonfly/server/player/skin"
+	"github.com/df-mc/dragonfly/server/world"
 	"github.com/google/uuid"
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
@@ -36,7 +37,7 @@ func (l *sessionList) Add(s *Session) {
 	l.sendSessionTo(s, s)
 }
 
-func (l *sessionList) Remove(s *Session) {
+func (l *sessionList) Remove(s *Session, entity world.Entity) {
 	l.mu.Lock()
 	others := slices.Clone(l.s)
 	l.s = sliceutil.DeleteVal(l.s, s)
@@ -45,6 +46,15 @@ func (l *sessionList) Remove(s *Session) {
 	// Unsend outside the lock to avoid blocking list operations on I/O.
 	for _, other := range others {
 		l.unsendSessionFrom(s, other)
+	}
+
+	if entity == nil {
+		return
+	}
+	for _, other := range others {
+		if other.viewLayer != nil {
+			other.viewLayer.Remove(entity)
+		}
 	}
 }
 