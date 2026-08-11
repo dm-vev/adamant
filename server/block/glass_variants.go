a47c08a16b29a2b7a958a12259e4574940c5a7ec
MEMOxiiii <189345814+MEMOxiiii@users.noreply.github.com>
2026-08-11 23:10:44 +0300

    server/block: Implement Tinted Glass (#1322)

--- server/block/glass_variants.go
+++ server/block/glass_variants.go
@@ -99,33 +99,6 @@ func (p HardenedStainedGlassPane) EncodeBlock() (string, map[string]any) {
 	return "minecraft:hard_" + p.Colour.String() + "_stained_glass_pane", nil
 }
 
-// TintedGlass is a glass block that blocks light.
-type TintedGlass struct {
-	solid
-	transparent
-	clicksAndSticks
-}
-
-// LightDiffusionLevel ...
-func (TintedGlass) LightDiffusionLevel() uint8 {
-	return 15
-}
-
-// BreakInfo ...
-func (g TintedGlass) BreakInfo() BreakInfo {
-	return newBreakInfo(0.3, alwaysHarvestable, nothingEffective, silkTouchOnlyDrop(g))
-}
-
-// EncodeItem ...
-func (TintedGlass) EncodeItem() (name string, meta int16) {
-	return "minecraft:tinted_glass", 0
-}
-
-// EncodeBlock ...
-func (TintedGlass) EncodeBlock() (string, map[string]any) {
-	return "minecraft:tinted_glass", nil
-}
-
 // allHardenedStainedGlass returns all hardened stained glass block states.
 func allHardenedStainedGlass() (blocks []world.Block) {
 	for _, c := range item.Colours() {
